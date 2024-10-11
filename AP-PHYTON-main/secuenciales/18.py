3cbdac548c2605250100ee825308fa3a5d25f696
maricielo <maricielocamilapalaciosquispe@gmail.com>
2024-10-11 16:38:44 -0500

    py

--- AP-PHYTON-main/secuenciales/18.py
+++ AP-PHYTON-main/secuenciales/18.py
@@ -5,9 +5,9 @@ cantidad = int ( input ( "Cantidad de unidad: ") )
 Punitario = int ( input ( "Precio unitario: ") )
 
 importe = cantidad * Punitario
-descuento1 = importe * 0.15
-descuento2 = ( importe - descuento1 ) * 0.15
-Paga = importe - descuento1 - descuento2
+descuento1 = 0.15 * importe
+descuento2 = 0.15 * ( importe - descuento1 )
+Paga = descuento1 + descuento2
 
 print ( f"importe: ${importe:.2f}" )
 print ( f"Total: ${descuento1 + descuento2:.2f}" )
