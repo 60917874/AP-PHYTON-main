a26a9b7f911f95ae27099b2b946e06baa3e8d90c
maricielo <maricielocamilapalaciosquispe@gmail.com>
2024-10-10 23:57:45 -0500

    py

---
+++ AP-PHYTON-main/secuenciales/20.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+cantidad = int ( input ( "Dinero en soles: ") )
+
+ByM = [200,100,50,20,10,5,2,1]
+
+desglose = {}
+
+for valor in ByM:
+    if cantidad >= valor:
+        billetes = cantidad // valor
+        desglose[valor] = billetes
+        cantidad -= billetes * valor
+
+for valor, cantidad in desglose.items():
+    if valor >= 10:
+        print ( f"{cantidad} billetes de S/. {valor}" )
+    else:
+        print ( f"{cantidad} monedas de S/. {valor}" )
