4b26fabbe0c4767735deebf99d64ad040681b964
maricielo <maricielocamilapalaciosquispe@gmail.com>
2024-10-13 18:30:24 -0500

    py

---
+++ AP-PHYTON-main/condicionales/19.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+sexo = input ( "Sexo: " ).upper()
+edad = int ( input ( "Edad: ") )
+
+if sexo == "F":
+    if edad < 23:
+        categoria = "FA"
+    else:
+        categoria = "FB"
+elif sexo == "M":
+    if edad < 25:
+        categoria = "MA"
+    else:
+        categoria = "MB"
+else:
+    categoria = "Error."
+    
+print ( f"Categoria: {categoria}")
