835152eea33eeebc5fd25d70ff00983624a8df2b
Maricielo <maricielocamilapalaciosquispe@gmail.com>
2024-10-19 00:28:17 -0500

    py

---
+++ AP-PHYTON-main/repetitivos/02.py
@@ -0,0 +1,18 @@
+import os
+os.system("cls")
+
+def multiplicar (n1, n2):
+    producto = 0
+    while n2 > 0:
+        n2 -= 1
+        producto += n1
+        return producto
+
+def multiplica (n1, n2):
+    if n2 == 1: return n1
+    return multiplica (n1, n2 - 1)
+
+multiplicando = int (input ( "Multiplicando: ") )
+multiplicador = int ( input ( "Multiplicador: ") )
+
+print (f"Producto = {multiplicar (multiplicando)}" )
