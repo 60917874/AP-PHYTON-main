2bfdeba073e45734a77b4ac57b4b6c40d3f03505
60917874 <maricielocamilapalaciosquispe@gmail.com>
2024-11-18 01:05:38 -0500

    py

--- AP-PHYTON-main/recursividad/03.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system("cls")
-
-def contarDigitos (n):
-    if n < 10:
-        return 1
-    else: return 1 + contarDigitos (n // 10)
-
-print (contarDigitos (12345))
--- AP-PHYTON-main/secuenciales/10.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system("cls")
-
-numero = input ( "numero natural: ")
-
-invertido = numero[::-1]
-
-print ( f"invertido: {invertido} ")
